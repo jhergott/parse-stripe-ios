c7cff623ece1a8e2f0713942df54bb3e64160a3d
Jake Hergott <jakehergott@gmail.com>
2015-04-08 10:47:35 -0500

    parse and frameworks working

---
+++ ParseStripe/ParseStripe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseStripe
+//
+//  Created by Jake Hergott on 4/8/15.
+//  Copyright (c) 2015 Jake Hergott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ParseStripe/ParseStripe/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  ParseStripe
+//
+//  Created by Jake Hergott on 4/8/15.
+//  Copyright (c) 2015 Jake Hergott. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Parse/Parse.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
